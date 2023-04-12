e619b0631656c710a6303460ca4c80044d6abaf7
Patrick Leary <patrick.r.leary@gmail.com>
2023-04-12 16:06:22 -0400

    add sound model; observations response return instances of sounds and comments

--- build/inaturalistjs.js
+++ build/inaturalistjs.js
@@ -48,6 +48,7 @@ module.exports = {
   ProjectUser: __webpack_require__(46),
   ProviderAuthorization: __webpack_require__(48),
   Site: __webpack_require__(53),
+  Sound: __webpack_require__(59),
   Taxon: __webpack_require__(18),
   User: __webpack_require__(20),
   FileUpload: __webpack_require__(58)
@@ -3410,7 +3411,11 @@ var Taxon = __webpack_require__(18);
 
 var Photo = __webpack_require__(19);
 
+var Sound = __webpack_require__(59);
+
 var Identification = __webpack_require__(27);
+
+var Comment = __webpack_require__(16);
 
 var Observation = /*#__PURE__*/function (_Model) {
   _inherits(Observation, _Model);
@@ -3443,6 +3448,18 @@ var Observation = /*#__PURE__*/function (_Model) {
     if (_this.photos && _this.photos.length > 0) {
       _this.photos = _this.photos.map(function (p) {
         return new Photo(p);
+      });
+    }
+
+    if (_this.sounds && _this.sounds.length > 0) {
+      _this.sounds = _this.sounds.map(function (s) {
+        return new Sound(s);
+      });
+    }
+
+    if (_this.comments && _this.comments.length > 0) {
+      _this.comments = _this.comments.map(function (c) {
+        return new Comment(c);
       });
     }
 
@@ -5501,6 +5518,61 @@ var FileUpload = /*#__PURE__*/_createClass(function FileUpload(attrs) {
 });
 
 module.exports = FileUpload;
+
+/***/ }),
+/* 59 */
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _get() { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(arguments.length < 3 ? target : receiver); } return desc.value; }; } return _get.apply(this, arguments); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Model = __webpack_require__(10);
+
+var Sound = /*#__PURE__*/function (_Model) {
+  _inherits(Sound, _Model);
+
+  var _super = _createSuper(Sound);
+
+  function Sound() {
+    _classCallCheck(this, Sound);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(Sound, null, [{
+    key: "typifyInstanceResponse",
+    value: function typifyInstanceResponse(response) {
+      return _get(_getPrototypeOf(Sound), "typifyInstanceResponse", this).call(this, response, Sound);
+    }
+  }]);
+
+  return Sound;
+}(Model);
+
+module.exports = Sound;
 
 /***/ })
 /******/ 	]);
